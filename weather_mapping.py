f1494331dd41336699965dff4392c5920dea006f
Fuglede30 <161837424+Fuglede30@users.noreply.github.com>
2025-05-13 21:26:21 +0200

    Add files via upload

---
+++ weather_mapping.py
@@ -0,0 +1,74 @@
+weather_map = {
+    'Clear': 'Clear',
+    'Fair': 'Clear',
+    'Partly Cloudy': 'Clear',
+    'Mostly Cloudy': 'Clear',
+    'Scattered Clouds': 'Clear',
+
+    'Cloudy': 'Cloudy',
+    'Overcast': 'Cloudy',
+    'Cloudy / Windy': 'Cloudy',
+    'Fair / Windy': 'Cloudy',
+    'Partly Cloudy / Windy': 'Cloudy',
+    'Mostly Cloudy / Windy': 'Cloudy', 
+
+    'Light Rain': 'Rain',
+    'Rain': 'Rain',
+    'Heavy Rain': 'Rain',
+    'Rain / Windy': 'Rain',
+    'Light Drizzle': 'Rain',
+    'Heavy Rain / Windy': 'Rain',
+    'Light Rain / Windy': 'Rain',
+    'Light Rain with Thunder': 'Rain',
+    'Drizzle and Fog': 'Rain',
+    'Light Freezing Rain': 'Rain',
+    'Light Freezing Drizzle': 'Rain',
+    'Light Drizzle / Windy': 'Rain',
+
+
+    'Thunderstorm': 'Thunderstorm',
+    'Thunderstorms and Rain': 'Thunderstorm',
+    'Heavy Thunderstorms and Rain': 'Thunderstorm',
+    'T-Storm': 'Thunderstorm',
+    'Thunder': 'Thunderstorm',
+    'Thunder / Windy': 'Thunderstorm',
+    'Heavy T-Storm / Windy': 'Thunderstorm',
+    'Thunder in the Vicinity': 'Thunderstorm',
+    'Light Thunderstorms and Rain': 'Thunderstorm',
+    'Heavy T-Storm': 'Thunderstorm',
+    'Squalls': 'Thunderstorm',
+
+
+    'Snow': 'Snow',
+    'Light Snow': 'Snow',
+    'Heavy Snow': 'Snow',
+    'Snow / Windy': 'Snow',
+    'Heavy Snow / Windy': 'Snow',
+    'Blowing Snow': 'Snow',
+    'Snow and Sleet / Windy': 'Snow',
+    'Light Snow / Windy': 'Snow',
+    'Light Snow and Sleet':'Snow',
+    
+
+    'Sleet': 'Sleet/Ice',
+    'Wintry Mix': 'Sleet/Ice',
+    'Wintry Mix / Windy': 'Sleet/Ice',
+    'Ice Pellets': 'Sleet/Ice',
+    'Light Ice Pellets': 'Sleet/Ice',
+    'Ice Pellets / Windy': 'Sleet/Ice',
+
+    'Fog': 'Fog',
+    'Fog / Windy': 'Fog',
+    'Mist': 'Fog',
+    'Patches of Fog': 'Fog',
+    'Haze': 'Fog',
+    'Haze / Windy': 'Fog',
+    'Shallow Fog': 'Fog',
+    'Light Freezing Fog': 'Fog',
+
+
+    'Smoke': 'Dust/Smoke',
+    'Blowing Dust': 'Dust/Smoke',
+
+    'N/A Precipitation': 'Unknown'
+}
